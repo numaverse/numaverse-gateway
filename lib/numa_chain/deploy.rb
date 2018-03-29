0a05b827afcc669f14b03f765d031ae3c7257b9a
hank <hstove@gmail.com>
2018-03-28 19:57:16 -0700

    save contract tx to

--- lib/numa_chain/deploy.rb
+++ lib/numa_chain/deploy.rb
@@ -8,6 +8,7 @@ module NumaChain
         contract_address = contract.deploy_and_wait
         creation_tx = Transaction.make_by_address(contract.deployment.id)
         contract = Contract.update_address :numa, contract_address, creation_tx
+        creation_tx.update(to_account: Account.make_by_address(contract.hash_address))
       rescue => e
         retry unless (tries -= 1).zero?
         raise e
