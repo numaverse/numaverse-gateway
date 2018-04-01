c6f3c8ed25f0ec4f6adcc75484b440c8f7d22bf0
hank <hstove@gmail.com>
2018-03-31 18:56:35 -0700

    basic eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "extends": [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  rules: {
+    'semi': 1,
+    'no-console': 0,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    jquery: true,
+  }
+};
